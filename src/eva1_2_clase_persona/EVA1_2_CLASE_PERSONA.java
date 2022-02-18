979235f09f11ddab9c6e3947fcfc9e6308948cb0
IvanTron <99278095+IvanRoger20@users.noreply.github.com>
2022-02-18 09:54:17 -0700

    Update EVA1_2_CLASE_PERSONA.java

--- src/eva1_2_clase_persona/EVA1_2_CLASE_PERSONA.java
+++ src/eva1_2_clase_persona/EVA1_2_CLASE_PERSONA.java
@@ -10,6 +10,22 @@ public class EVA1_2_CLASE_PERSONA {
         estudiante.id = "21550379";
         estudiante.nombre = "Juan Perez Jolote";
         estudiante.edad = 18;
+        estudiante.imprimirDatos();
+        System.out.println("Año de nacimiento: " + estudiante.calcularAnioNacimiento());
+        
+        Persona estudiante2 = new Persona();
+        estudiante2.nombre = "Ruben";
+        estudiante2.id = "112133";
+        estudiante2.edad = 40;
+        estudiante2.imprimirDatos();
+        System.out.println("Año de nacimiento: " + estudiante2.calcularAnioNacimiento());
+        
+        Persona estudiante3 = new Persona();
+        estudiante3.nombre = "Pedro";
+        estudiante3.id = "12412334";
+        estudiante3.edad = 30;
+        estudiante3.imprimirDatos();
+        System.out.println("Año de nacimiento: " + estudiante3.calcularAnioNacimiento());
     }
 
 }
@@ -20,5 +36,18 @@ class Persona {
     String nombre;
     int edad;
     
+    void imprimirDatos() {
+        
+        System.out.println("El nombre es: " + nombre);
+        System.out.println("Su id es: " + id);
+        System.out.println("Su edad es: " + edad);  
+        
+    }
     
+    int calcularAnioNacimiento() {
+        
+        int anioNac = 2022 - edad;
+        return anioNac;
+        
+    }
 }
